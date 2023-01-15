651130c14a00bf0d0684bec59e931ca8e41a19d9
Geovane Calheira da Silva <geovanecalheiro@gmail.com>
2023-01-14 21:01:05 -0300

    Project backend created

---
+++ backend/src/main/java/com/gtecnologia/GTmovies/GtMoviesApplication.java
@@ -0,0 +1,13 @@
+package com.gtecnologia.GTmovies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GtMoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GtMoviesApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/gtecnologia/GTmovies/GtMoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gtecnologia.GTmovies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GtMoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
