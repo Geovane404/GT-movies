0fc91395adf3c3e3fbe83eaf472efb3f879e1b18
Geovane Calheira da Silva <geovanecalheiro@gmail.com>
2023-01-19 17:20:54 -0300

    AppConfig

---
+++ backend/src/main/java/com/gtecnologia/GTmovies/config/AppConfig.java
@@ -0,0 +1,35 @@
+package com.gtecnologia.GTmovies.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+@Configuration
+public class AppConfig {
+	
+	@Value("${jwt.secret}")
+	private String jwtSecret;
+
+	@Bean
+	public BCryptPasswordEncoder passwordEncode() {
+
+		return new BCryptPasswordEncoder();
+	}
+	
+	//JWT
+	@Bean
+	public JwtAccessTokenConverter accessTokenConverter() {
+		
+		JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
+		tokenConverter.setSigningKey(jwtSecret);
+		return tokenConverter;
+	}
+	
+	@Bean
+	public JwtTokenStore tokenStore() {
+		return new JwtTokenStore(accessTokenConverter());
+	}
+}
